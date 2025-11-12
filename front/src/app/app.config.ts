82ce0a52ec25a174de15c98793784ea1d9a963fb
Juan Costa <juancostasil16@gmail.com>
2025-11-12 17:34:47 -0300

    Corrigindo arquivo app.config

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -3,12 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
-import { provideHttpClient } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-    provideHttpClient(),
-    importProvidersFrom(ReactiveFormsModule)
+
   ]
 };
